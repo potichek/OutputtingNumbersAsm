cb87f2b92adcc9412d4431fea7538eddd7d87905
potichek <112338597+potichek@users.noreply.github.com>
2024-02-07 19:00:00 +0300

    Update build.bat
    Change link options in build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 nasm -f win64 main.asm -o main.o
 nasm -f win64 arraylist.asm -o arraylist.o
-ld main.o arraylist.o -o main.exe -l kernel32 -l user32
+ld main.o arraylist.o -o main.exe -l kernel32
 main.exe
